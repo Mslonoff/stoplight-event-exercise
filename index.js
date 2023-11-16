f02abcb3fe3c27db1a2174da40b97ea428496f11
Mslonoff <matthew.slonoff@gmail.com>
2023-11-16 18:22:57 -0500

    Improved

--- index.js
+++ index.js
@@ -1,63 +1,29 @@
 (function() {
-  'use strict';
-  var counterYellow = 0;
-  var counterRed = 0;
-  var counterGreen = 0;
-
-let stopButton = document.getElementById('stopButton');
-let stopLight = document.getElementById('stopLight');    
+let isStopOn = false;
+let isSlowOn = false;
+let isGoOn = false;
+let stopButton = document.querySelector('#stopButton');
+let stopLight = document.querySelector('#stopLight');    
+let slowButton = document.querySelector('#slowButton');
+let slowLight = document.querySelector('#slowLight');
+let goButton = document.querySelector('#goButton');
+let goLight = document.querySelector('#goLight');
 
 stopButton.addEventListener('click', function () {
-      counterRed++
-      let isStopOn;
-      // if counter is odd
-      if(counterRed % 2 === 1) {
-        // light should be on
-        isStopOn = true;
-        // else
-      } else {
-        // light is turned off
-        isStopOn = false;
-      }
+  isStopOn = !isStopOn;
       stopLight.style.backgroundColor = isStopOn ? 'red' : 'black';
     });
 
-let slowButton = document.getElementById('slowButton');
-let slowLight = document.getElementById('slowLight');
-// add event listener for slow button and go button that makes the light the right color
-
 slowButton.addEventListener('click', function () {
-    console.log('click', 'slowButton');
-    counterYellow++
-    let isSlowOn;
-    // if counter is odd
-    if(counterYellow % 2 === 1) {
-      // light should be on
-      isSlowOn = true;
-      // else
-    } else {
-      // light is turned off
-      isSlowOn = false;
-    }
+  isSlowOn = !isSlowOn;
+  console.log('click', 'goButton');
     slowLight.style.backgroundColor = isSlowOn ? 'yellow' : 'black';
-  });
+  }
+);
 
-  let goButton = document.getElementById('goButton');
-  let goLight = document.getElementById('goLight')
-  
 goButton.addEventListener('click', function () {
+  isGoOn = !isGoOn;
       console.log('click', 'goButton');
-      counterGreen++
-      let isGoOn;
-      // if counter is odd
-      if(counterGreen % 2 === 1) {
-        // light should be on
-        isGoOn = true;
-        // else
-      } else {
-        // light is turned off
-        isGoOn = false;
-      }
       goLight.style.backgroundColor = isGoOn ? 'green' : 'black';
   });
 
