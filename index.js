ab7a339df810b073f20a8c590cb7f137a00c69b7
Mslonoff <matthew.slonoff@gmail.com>
2023-11-16 19:35:43 -0500

    Part3

--- index.js
+++ index.js
@@ -27,4 +27,55 @@ goButton.addEventListener('click', function () {
       goLight.style.backgroundColor = isGoOn ? 'green' : 'black';
   });
 
+  let enteredGoButton = true;
+  goButton.addEventListener("mouseover", (event) => {
+    enteredGoButton = !enteredGoButton;
+    if (enteredGoButton) {
+      console.log(`Enter goButton`);
+    } else {
+      console.log(`Left goButton`);
+    }
+  });
+  
+  // create a variable set to false representing the mouse entering the space on page where stopButton is located
+  let enteredStopButton = true;
+  stopButton.addEventListener("mouseover", (event) => {
+    enteredStopButton = !enteredStopButton;
+    if (enteredStopButton) {
+      console.log(`Enter stopButton`);
+    } else {
+      console.log(`Left stopButton`);
+    }
+  });
+  
+  // create a variable set to false representing the mouse entering the space on page where slowButton is located
+  let enteredSlowButton = true;
+  slowButton.addEventListener("mouseover", (event) => {
+    enteredSlowButton = !enteredSlowButton;
+    if (enteredSlowButton) {
+      console.log(`Enter slowButton`);
+    } else {
+      console.log(`Left slowButton`);
+    }
+  });
+  
 })();
+
+// mouseTarget.addEventListener("mouseenter", (e) => {
+//   mouseTarget.style.border = "5px dotted orange";
+//   enterEventCount++;
+//   addListItem(`This is mouseenter event ${enterEventCount}.`);
+// });
+// Add new DOM event listeners and handlers to log the mouse state of each button.
+// If a user's mouse hovers over a button,
+// create a variable set to false representing the mouse entering the space on page where goButton is located
+
+// log "Entered <textContent> button" to the console.
+// When a user's mouse stops hovering over a button, 
+// goButton.addEventListener("mouseleave", (event) => {
+
+//   console.log(`Left goButton`);
+// });
+
+// log "Left <textContent> button" to the console.
+// TIP: Each event type will need a separate event listener.
