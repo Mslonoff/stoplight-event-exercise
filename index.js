e88c68e7302e1f9bcbdea0f3041c763fe3fe2fb6
Mslonoff <matthew.slonoff@gmail.com>
2023-11-17 10:44:30 -0500

    part2Good

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let slowLight = document.querySelector('#slowLight');
 let goButton = document.querySelector('#goButton');
 let goLight = document.querySelector('#goLight');
 
+
 stopButton.addEventListener('click', function () {
   isStopOn = !isStopOn;
       stopLight.style.backgroundColor = isStopOn ? 'red' : 'black';
@@ -16,47 +17,43 @@ stopButton.addEventListener('click', function () {
 
 slowButton.addEventListener('click', function () {
   isSlowOn = !isSlowOn;
-  console.log('click', 'goButton');
     slowLight.style.backgroundColor = isSlowOn ? 'yellow' : 'black';
   }
 );
 
 goButton.addEventListener('click', function () {
   isGoOn = !isGoOn;
-      console.log('click', 'goButton');
       goLight.style.backgroundColor = isGoOn ? 'green' : 'black';
   });
 
-  let enteredGoButton = true;
-  goButton.addEventListener("mouseover", (event) => {
-    enteredGoButton = !enteredGoButton;
-    if (enteredGoButton) {
-      console.log(`Enter goButton`);
-    } else {
-      console.log(`Left goButton`);
-    }
+  goButton.addEventListener("mouseenter", (event) => {
+    console.log(`Enter goButton`);
   });
   
+  goButton.addEventListener('mouseleave', (event) => {
+    console.log(`Left goButton`);
+  });
+  //setup an addEventListener for "mouseenter", and "mouseleave" for all three buttons
+  //using arrow functions with (event), output a console log for the condition
+  //Bonus section: using original tenary operator function, create another line under with console message for "button on, and button off when clicked"
   // create a variable set to false representing the mouse entering the space on page where stopButton is located
-  let enteredStopButton = true;
-  stopButton.addEventListener("mouseover", (event) => {
-    enteredStopButton = !enteredStopButton;
-    if (enteredStopButton) {
-      console.log(`Enter stopButton`);
-    } else {
-      console.log(`Left stopButton`);
-    }
+
+  stopButton.addEventListener("mouseenter", (event) => {
+    console.log(`Enter stopButton`);
   });
   
+  stopButton.addEventListener('mouseleave', (event) => {
+    console.log(`Left stopButton`);
+  });
+
+    
   // create a variable set to false representing the mouse entering the space on page where slowButton is located
-  let enteredSlowButton = true;
-  slowButton.addEventListener("mouseover", (event) => {
-    enteredSlowButton = !enteredSlowButton;
-    if (enteredSlowButton) {
-      console.log(`Enter slowButton`);
-    } else {
-      console.log(`Left slowButton`);
-    }
+  slowButton.addEventListener("mouseenter", (event) => {
+    console.log(`Enter slowButton`);
+  });
+  
+  slowButton.addEventListener('mouseleave', (event) => {
+    console.log(`Left slowButton`);
   });
   
 })();
